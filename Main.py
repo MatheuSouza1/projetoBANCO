32bd6138e638b340ee8b6323a3cab865cba39aa3
Pqmeu <rdesouzamatheus@gmail.com>
2022-12-21 03:27:49 -0300

    Incrementando o projeto com um looping para saque, deposito e consulta de extrato

--- Main.py
+++ Main.py
@@ -1,10 +1,27 @@
 from Cliente import Cliente
 from Conta import Conta
 
-c1 = Cliente("Matheus", "22998662757")
-conta = Conta(c1.get_nome(), 1222)
+print("CRIE SUA CONTA NO BANCO:")
+nome = input("Digite seu nome: ")
+num = input("Digite seu numero de telefone: ")
+cliente = Cliente(nome, num)
+conta = Conta(cliente.get_nome(), 1234)
 
-conta.deposito(2000)
+print("Conta criada:")
 conta.extrato()
-conta.saque(50)
-conta.extrato()
+
+while True:
+    print("Digite o numero da ação desejada: \n[1]Ver extrato\n[2]Fazer deposito\n[3]Saque\n[0]Sair")
+    r = input("")
+    if r == "0":
+        break
+    elif r == "1":
+        conta.extrato()
+    elif r == "2":
+        dep = float(input("Quanto você quer depositar? "))
+        conta.deposito(dep)
+    elif r == "3":
+        saque = float(input("Quanto você quer sacar? "))
+        conta.saque(saque)
+    else:
+        print("Digite uma opção válida")
